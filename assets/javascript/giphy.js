d0488059d8d6d988bc8a44858520dd2660cf11c9
HaifaSeyam <h.abuamro@hotmail.com>
2019-01-13 15:38:01 -0600

    static and dynamic gifs and final application

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -40,6 +40,22 @@ function displayTopicInfo() {
             // Putting the entire topic above the previous topics
             $(".topics-view").prepend(imageDiv);
         }
+
+        // This function handles events where a topic button is clicked
+        $(".gif").on("click", function() {
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+              $(this).attr("src", $(this).attr("data-animate"));
+              $(this).attr("data-state", "animate");
+            } else {
+              $(this).attr("src", $(this).attr("data-still"));
+              $(this).attr("data-state", "still");
+            }
+          });
     });
 }
 
